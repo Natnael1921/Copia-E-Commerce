94da218ee689bb4795f9d4597505e89496d1a75a
Natnael Mekonnen <nnatnaelmekonnen19@gmail.com>
2026-03-05 12:05:26 +0300

    Implement authentication

--- Backend/app.js
+++ Backend/app.js
@@ -1,17 +1,25 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
-import routes from "./routes/index.js";
+
+import authRoutes from "./routes/auth.routes.js";
 import errorMiddleware from "./middleware/error.middleware.js";
 
 const app = express();
 
-// Core Middlewares
+// Core middleware
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
 
-// API 
-app.use("/api/v1", routes);
+// Routes
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ message: "API working " });
+});
+
+// Error middleware
 app.use(errorMiddleware);
+
 export default app;
---
+++ Backend/controllers/auth.controller.js
@@ -0,0 +1,68 @@
+import bcrypt from "bcryptjs";
+import User from "../models/User.model.js";
+import generateToken from "../utils/generateToken.js";
+
+// Register
+export const register = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const token = generateToken(user);
+
+    res.status(201).json({
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Login
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = generateToken(user);
+
+    res.json({
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
